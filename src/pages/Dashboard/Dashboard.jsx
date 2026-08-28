49816f4370628ff7f6784b46c2abcdc96d5249af
Srinath Vatchavari Venkateshan <166697624+vvsrinath@users.noreply.github.com>
2026-08-29 02:41:15 +0530

    Delete src directory

--- src/pages/Dashboard/Dashboard.jsx
+++
@@ -1,9 +0,0 @@
-export default function Dashboard() {
-  return (
-    <main className="flex-1 flex flex-col items-center justify-center p-6 text-center">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-text">
-        This is Dashboard Page
-      </h1>
-    </main>
-  );
-}
--- src/pages/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex-1 flex flex-col items-center justify-center p-6 text-center">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-text">
-        This is Home Page
-      </h1>
-    </main>
-  );
-}
